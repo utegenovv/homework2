1d44a69421394e46545625a92cc6488af930000c
vagap <you@example.com>
2022-12-23 23:23:19 +0300

    homework2

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// 1 задача
+/*Console.Write("Велите трехзначное число :");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1000)
+{
+    int sot = num % 100;
+    int ed = sot / 10 ;
+    Console.WriteLine(ed);
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Выйди и зайди нормально" );
+    Console.ForegroundColor = ConsoleColor.White;
+}
+*/
+
+// 2 задача
+/*
+Console.Write("Велите трехзначное число :");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Третьей цифры нет." );
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+else
+{
+    int sot = num % 10;
+    Console.WriteLine(sot);
+}
+*/
+//3 задача 
+/*
+Console.Write("Введите день недели в виде числа от 1 до 7    :" );
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 1 && num <= 5) 
+{ 
+    Console.Write(" Нет, это не выходной еще работаем");
+} 
+    else if (num >= 6 && num <= 7) 
+{ 
+    Console.Write(" Да, это выходной ураааааааа"); 
+} 
+else Console.Write(" Не существует такого дня недели"); 
+*/
